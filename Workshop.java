4b81640c56f27215f8c2e65ebc9d475937a1fa1b
Kasi <116343452+kasikasikasi@users.noreply.github.com>
2025-02-12 14:22:49 +0100

    Update Workshop.java

--- Workshop.java
+++ Workshop.java
@@ -1,32 +1,38 @@
+
+package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Workshop<T>{
+public class Workshop<T extends Car>{
 
     private int capacity;
-    private T model;
     private List<T> workshopList= new ArrayList<>(capacity);
+    private Class<T> type;
 
-    public Workshop(int capacity, T model){
+    public Workshop(int capacity, Class<T> type){
         this.capacity = capacity;
-        this.model = model;
+        this.type = type;
     }
 
-    public void fixCar(T damagedGood){
-        if ( workshopList.size() < capacity){
-        workshopList.add(damagedGood);
-    }
+    public <S extends Car> void fixCar(S damagedCar){
+        if (!type.isInstance(damagedCar)) {
+            throw new IllegalArgumentException("This workshop only accepts " + type.getSimpleName() + " cars!");
+        }
+        if ( workshopList.size() < capacity) {
+            workshopList.add((T) damagedCar);
+        }
     } 
 
     public T removeCar(T car) {
+        if (!workshopList.contains(car)) {
+            System.out.println("This car is not in the workshop");
+            return null;
+        }
         workshopList.remove(car);
         return car;
 
     }
 
-    public T getmodelname() {
-        return this.model;
-    }
 
     public List<T> getWorkshopList() {
         return new ArrayList<>(workshopList); 
