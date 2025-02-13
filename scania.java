e60ef76989bb437f437118e23a9e3880fe4b2b51
Zimpan3000 <simon.radberg@gmail.com>
2025-02-13 11:27:52 +0100

    Update scania.java

--- scania.java
+++ scania.java
@@ -1,60 +1,52 @@
-
-package org.example;
 import java.awt.*;
-public class Scania extends Car implements LoadFunctionality {
-    private final double velocityfatcor = 1.1;
+
+
+public class Scania extends Truck implements loadFunctionality {
     private double angle = 0.0;
-    private int maxAngle = 70;
-    private int minAngle = 0;
+    private int maxangle = 70;
+    private int minangle = 0;
 
     public Scania(){
-        super(2,0.9,0,Color.MAGENTA, "scania",6);
+        super(2,0.9,10,Color.MAGENTA, "scania",6);
     }
-    @Override protected double speedFactor(){
-            return getEnginePower()*0.01*velocityfatcor;
+
+    
+    @Override
+    public void raiseAngle(double incrementdegrees) {
+       if (getCurrentSpeed() == 0 && angle + incrementdegrees <= maxangle && incrementdegrees > minangle ){
+        if (currentSpeed == 0) {
+        angle = incrementdegrees+ angle;
+        fullyOpen();
         }
+    }
+    }
+
 
     @Override
-    public void raise(double incrementDegrees){
-    if (getCurrentSpeed() == 0 && angle + incrementDegrees <= maxAngle && incrementDegrees >= minAngle ){
+    public void lowerAngle(double decrementdegrees) {
+        if (getCurrentSpeed() == 0 && angle - decrementdegrees >= minangle && decrementdegrees < maxangle ){
+            angle = angle - decrementdegrees;
+            if (angle <= 0) {
+                fullyClose();
+            }
 
-        angle = incrementDegrees + angle;
-    }
-        
-    }
-
-    @Override
-    public void lower(double decrementDegrees){
-    if (getCurrentSpeed() == 0 && angle - decrementDegrees >= minAngle && decrementDegrees < maxAngle ){
-        angle = angle - decrementDegrees;
-    }
-        
+        }
     }
 
-    @Override
-    public double getAngle(){
+    public double getangle(){
         return angle;
     }
 
-    @Override
-    public void gas(double amount) {
-        if (amount > 0 && amount < 1 && getAngle() == 0) {
-                    incrementSpeed(amount);
-        }
-    }
-    @Override
-    public void brake(double amount) {
-        if (amount > 0 && amount < 1 && getAngle() == 0) {
-                decrementSpeed(amount);
-        }
+   
+    public int getmaxangle(){
+        return maxangle;
     }
 
-    public int getMaxAngle(){
-        return maxAngle;
+    public int getminangle(){
+        return minangle;
     }
 
-    public int getMinAngle(){
-        return minAngle;
-    }
+
+  
 
 }
