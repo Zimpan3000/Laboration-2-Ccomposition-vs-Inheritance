f9761b64d21ec8f90e6f530f759fdf87db39aff3
Zimpan3000 <simon.radberg@gmail.com>
2025-02-13 11:09:58 +0100

    Update TMF.java

--- TMF.java
+++ TMF.java
@@ -1,45 +1,43 @@
-package org.example;
+
 import java.awt.*;
 import java.util.List;
 import java.util.ArrayDeque;
 import java.util.Deque;
-public class TMF extends Car implements RampFunctionality {
-    private final double fForce = 1.3;
-    private boolean rampIsDown = true;
-    private int maxsize = 5;
+public class TMF extends Truck{
+
+    private int maxsize;
     private Deque<Car> carlist = new ArrayDeque<>(maxsize);
     private int widthOfLorry= 4;
-    
-    public TMF(){
+   
+
+    public TMF(int maxsize){
         super(2, 0.8, 0,Color.CYAN ,"TMF",4);
-        
+        this.maxsize = maxsize;
     }
 
-    private Boolean isCarClose(Car car) {
-        Double diffX = Math.abs(car.getPositionX() - getPositionX());
-        Double diffY = Math.abs(car.getPositionY() - getPositionY());
-        Double diffXY = diffY + diffX;
-        return(diffXY > 0 && diffXY <= 2);
-    }
-
+    
     public void loadCars(List<Car> loadList){
-
         if (loadList.size() + carlist.size() <= maxsize){
-            for (Car car : loadList) {
-                if (car.getWidthOfCar() <= widthOfLorry && !(car instanceof TMF)) {
-                    if (isCarClose(car)) {
-                        carlist.addFirst(car);
-                       /* car.addPositionX(getPositionX());
-                        car.addPositionY(getPositionY());*/
+            for (int i = 0 ; i < loadList.size(); i++ ){
+                if (loadList.get(i).getwidthOfCar() <= widthOfLorry && !(this instanceof Truck)&& isCarClose(loadList.get(i))) {
+                    if (!loadList.get(i).getOutOfOrder()) {
+                        carlist.addFirst(loadList.get(i));
+                        loadList.get(i).setOutOfOrderOn();
                     }
                 }
             }
         }
     }
 
-
+    private Boolean isCarClose(Car car) {
+        Double diffX = Math.abs(car.getPositionX() - getPositionX());
+        Double diffY = Math.abs(car.getPositionY() - getPositionY());
+        return(10 >= diffX && diffX > 1 && diffY > 1 && diffY <= 10);
+    }
+    
     public Car unloadCars() {
-            return carlist.removeFirst();
+        carlist.peek().setOutOfOrderOff();
+        return carlist.removeFirst();
         
     }
     @Override
@@ -48,27 +46,7 @@ public class TMF extends Car implements RampFunctionality {
         for (Car car : carlist) {
             car.addPositionX(getPositionX());
             car.addPositionY(getPositionY());
-
             }
-    }
-
-   @Override 
-   public void gas(double amount) {
-        if (amount > 0 && amount < 1 && !rampIsDown) {
-                incrementSpeed(amount);
-        }
-    }
-
-    @Override
-    public void changeRampIsDown(boolean status){
-       if(currentSpeed == 0){ rampIsDown = status;}
-    }
-
-    @Override protected double speedFactor(){
-        return getEnginePower()*0.01*fForce;
-    }
-    public boolean getRampIsDown(){
-        return rampIsDown;
     }
 
     public int getmaxsize(){
